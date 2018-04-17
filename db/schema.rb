db533e2aa83c431d74f787bde50bbe77feaec673
Jeff Wong <jeffmjwong@gmail.com>
2018-04-17 13:09:30 +1000

    Bundle add gem Geocoder and generate migration for columns latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417015832) do
+ActiveRecord::Schema.define(version: 20180417030846) do
 
   create_table "authors", force: :cascade do |t|
     t.string "img"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20180417015832) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
