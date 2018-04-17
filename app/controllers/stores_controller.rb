aadba4a37d9d0ec401f99b6a96c3d50ae9ec040b
Jeff Wong <jeffmjwong@gmail.com>
2018-04-17 13:05:50 +1000

    Embed google map using actual full address of stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,9 @@
 class StoresController < ApplicationController
   def index
+    @stores = Store.all
+  end
+
+  def show
+    @store = Store.find(params[:store_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "/books/:book_id", to: "books#show", as: "book"
   get "/authors", to: "authors#index", as: "authors"
   get "/stores", to: "stores#index", as: "stores"
+  get "/stores/:store_id", to: "stores#show", as: "store"
   root to: "pages#home", as: "home"
 end
